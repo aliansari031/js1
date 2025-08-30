9e659a0d6b62cbf15d9c40540609e9b87aa6bcd2
aliansari031 <aliansari38648@gmail.com>
2025-08-30 08:07:19 -0700

    Add files via upload

---
+++ new folder3/index.js
@@ -0,0 +1,93 @@
+//*********************************************( assignment no # 1 )*************************************************
+
+//   ********************************{ Q No # 1 }*************************************
+
+/*var su = "S um";
+var num = 3;
+var number = 5;
+var number1 = 8;
+
+document.write(su , " of " , num , " and " , number ," is " , number1);*/
+
+ 
+
+//  ********************************{ Q No # 2 }*************************************
+
+ //                  *************        SUBTRACTION        ************
+
+
+ /*var num = 3;
+ var number = 5;
+ var number1 = 8;
+
+
+ var subtraction = num - number - number1;
+ 
+ document.write("subtraction of ", num, " and ", number, "and" , number1 , " is ", subtraction, "<br>"); */
+
+  //                  *************        MULTIPLICATION        ************
+
+/*var num = 3;
+ var number = 5;
+ var number1 = 8;
+
+
+ var multiplication = num * number * number1;
+ 
+ document.write("multiplication of ", num, " and ", number, " and ", number1 , " is " , multiplication, "<br>"); */
+
+  //                  *************        DIVISION        ************
+
+
+/*var num = 3;
+ var number = 5;
+ var number1 = 8;
+
+
+ var division = num / number / number1;
+ 
+ document.write("division of ", num, " and ", number, " and ", number1 , " is " , division, "<br>"); */
+
+  //                  *************        MODULUS        ************
+
+/*var num = 3;
+ var number = 5;
+ var number1 = 8;
+
+
+ var modulus = num % number % number1;
+ 
+ document.write("modulus of ", num, " and ", number, " and ", number1 , " is " , modulus, "<br>");
+*/
+
+  // ********************************{ Q No # 3 }*************************************
+  //    a 
+
+  document.write("Value after variable declaration is undefined", "<br>");
+
+
+   var nu = 5;
+   document.write("Initial value: " + nu , "<br>");
+   
+
+   //   b
+
+   var x = 5;
+   var y = x + 1;
+   document.write("Value after increment is: " + y , "<br>");
+   
+
+   //    c
+
+   var x = 6;
+   x = x + 7;
+   document.write("Value after addition is: " + x , "<br>");
+
+   //    D
+   
+   var x = 13;
+   x = x - 1;
+
+   document.write("Value after decrement is:" + x , "<br>");
+
+   document.write("The remainder is: 0");
